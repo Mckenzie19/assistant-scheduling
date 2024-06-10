cc5840c83abfcbe2fc1549a634fddcdcd2015b03
Mckenzie19 <mckenzie19shay@outlook.com>
2024-06-10 12:58:28 -0500

    Adjusting Communion Service Selection formatting

--- src/main/java/assistantscheduling/ui/CommunionFormPanel.java
+++ src/main/java/assistantscheduling/ui/CommunionFormPanel.java
@@ -1,11 +1,17 @@
 package assistantscheduling.ui;
 
 import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.ListCellRenderer;
 import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,7 +49,7 @@ public class CommunionFormPanel extends FormPanel {
 	 */
 	public CommunionFormPanel(DataFileCreator dfc) {
 		setName("Communion Selection Panel");
-		setLayout(new MigLayout("wrap 3", "[][grow][grow]", "[]10[grow]"));
+		setLayout(new MigLayout("wrap 2", "[grow][]", "[]10[grow]"));
 		setBackground(Color.WHITE);
 		setOpaque(true);
 		setFocusable(true);
@@ -58,22 +64,52 @@ public class CommunionFormPanel extends FormPanel {
 		
 		// Initialize panel components
 		lblTitle = new JLabel("Select Communion Sundays");
-		dateModel.addElement(LocalDate.now());
+		dateModel.addElement(LocalDate.now()); // Temporary data allows for the creation of the list model. This gets cleared upon data loading.
 		lstServices = new JList<LocalDate>(dateModel);
 		scrollPane = new JScrollPane(lstServices);
 		
 		// Set styles 
 		lblTitle.setFont(StyleSettings.TITLE_FONT);
 		lstServices.setFont(StyleSettings.CONTENT_FONT);
+		lstServices.setCellRenderer(getRenderer()); // Sets cell borders and fonts
+		lstServices.setFixedCellHeight(25);
+		lstServices.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		lstServices.setVisibleRowCount(-1);
+		// Makes sure each cell has the maximum space it can (assumes 4 columns)
+		int listWidth = lstServices.getVisibleRect().width;
+		lstServices.setFixedCellWidth(listWidth/4);
 		
 		// Set initial settings and content
-		lstServices.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		lstServices.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		lstServices.addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				if (e.getValueIsAdjusting() == false) {
+
+			        if (lstServices.getSelectedIndex() == -1) {
+			        	// Selection cleared
+			        } else {
+			        	// Cell selected, flip the Communion tag (on/off)
+			        	
+			        }
+			    }			
+			}
+		});
+		scrollPane.getViewport().setView(lstServices);
 		
 		// Add components to panel
 		add(lblTitle, "span 2,wrap");
 		add(scrollPane, "grow");
 	}
 
+	
+	/**
+	 * Given a start date, end date, and day of the week, returns a list of weekly dates occurring on the given day of week.
+	 * 
+	 * @param startDate - Inclusive start date of the range to return
+	 * @param endDate - Inclusive end date of the range to return
+	 * @param dayOfWeek - DayOfWeek value that indicates what day of the week to select
+	 */
 	public ArrayList<LocalDate> getListWeekly(LocalDate startDate, LocalDate endDate, DayOfWeek dayOfWeek) {
 		LocalDate firstActualDay = startDate.with(TemporalAdjusters.next(dayOfWeek));
 		LocalDate lastActualDay = endDate.with(TemporalAdjusters.previous(dayOfWeek));
@@ -85,6 +121,23 @@ public class CommunionFormPanel extends FormPanel {
 		}
 		return dateList;
 	}
+	
+	private ListCellRenderer<? super LocalDate> getRenderer() {
+        return new DefaultListCellRenderer(){
+            private static final long serialVersionUID = 1L;
+
+			@Override
+            public Component getListCellRendererComponent(JList<?> list,
+                    Object value, int index, boolean isSelected,
+                    boolean cellHasFocus) {
+                JLabel listCellRendererComponent = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected,cellHasFocus);
+                listCellRendererComponent.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0,Color.GRAY));
+                listCellRendererComponent.setFont(StyleSettings.CONTENT_FONT);
+                return listCellRendererComponent;
+            }
+        };
+    }
+
 	
 	@Override
 	public void checkData() throws Exception {
@@ -109,6 +162,14 @@ public class CommunionFormPanel extends FormPanel {
 		for (LocalDate date: dateList) {
 			dateModel.addElement(date);
 		}
+		
+		// Dynamically decides how many columns can fix maximum -- Kept in case the hard coded 4 columns doesn't work out
+		// Fix the cell width to better fill the space
+//		int listWidth = lstServices.getVisibleRect().width;
+//		int currCellWidth = lstServices.getCellBounds(0, 0).width;
+//		int numCols = listWidth / currCellWidth;
+//		int padding = (listWidth % currCellWidth) / numCols; // How much "padding" to add to the width of each cell to evenly distribute the white space
+//		lstServices.setFixedCellWidth(currCellWidth+padding);
 	}
 
 }
